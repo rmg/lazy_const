b67cf56f67a52d10dd6341f5bae1ec18d6c89bb8
Ryan Graham <rgraham@fortinet.com>
2013-03-13 13:05:30 -0700

    Initial code import

---
+++ lib/lazy_const.rb
@@ -0,0 +1,28 @@
+module LazyConst
+
+  CACHE = {}
+
+  def lazy_const(name, &block)
+    raise Error("lazy_const requires a block") unless block_given?
+    LazyConst::CACHE.delete "#{self.name}.#{name}"
+    define_singleton_method name do
+      LazyConst::CACHE["#{self.name}.#{name}"] ||= block.call
+    end
+  end
+
+  def self.preload
+    self.clear
+    LazyConst::CACHE.keys.each do |class_dot_name|
+      klass, name = class_dot_name.split('.')
+      m = klass.constantize.send name.to_sym
+    end
+  end
+
+  def self.clear
+    LazyConst::CACHE.keys.each do |k|
+      LazyConst::CACHE[k] = nil
+    end
+  end
+
+end
+
--- test/test_lazy_const.rb
+++ test/test_lazy_const.rb
@@ -1,7 +1,22 @@
 require 'helper'
 
+$called = 0
+class Foo
+  extend LazyConst
+  lazy_const :TEST do
+    $called += 1
+    42
+  end
+end
+
+
 class TestLazyConst < MiniTest::Unit::TestCase
-  def test_something_for_real
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_lazy_const
+    called = 0
+    assert_equal $called, 0
+    assert_equal Foo.TEST, 42
+    assert_equal $called, 1
+    assert_equal Foo.TEST, 42
+    assert_equal $called, 1
   end
 end
