6f7c44c42dd5620db7ce5b1cac4577d9cfbf080e
Ryan Graham <r.m.graham@gmail.com>
2013-03-13 18:00:04 -0700

    Clean up tests

--- test/test_lazy_const.rb
+++ test/test_lazy_const.rb
@@ -5,18 +5,35 @@ class Foo
   extend LazyConst
   lazy_const :TEST do
     $called += 1
-    42
+    new 42
+  end
+  attr_reader :number
+  def initialize(number)
+    @number = number
   end
 end
 
 
 class TestLazyConst < MiniTest::Unit::TestCase
+
+  def setup
+    $called = 0
+  end
+
   def test_lazy_const
-    called = 0
     assert_equal $called, 0
-    assert_equal Foo.TEST, 42
+    obj = Foo.TEST
+    assert_equal obj, Foo.TEST
     assert_equal $called, 1
-    assert_equal Foo.TEST, 42
+    assert_equal obj, Foo.TEST
     assert_equal $called, 1
+  end
+
+  def test_preload
+    assert_equal $called, 0
+    LazyConst.preload
+    assert_equal $called, 1
+    obj = Foo.TEST
+    assert_equal obj, Foo.TEST
   end
 end
