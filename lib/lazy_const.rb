4413d9b47de8dd4442dcb4962e3cb4a99090795f
Ryan Graham <r.m.graham@gmail.com>
2013-03-13 18:00:15 -0700

    Make tests pass again

--- lib/lazy_const.rb
+++ lib/lazy_const.rb
@@ -14,7 +14,7 @@ module LazyConst
     self.clear
     LazyConst::CACHE.keys.each do |class_dot_name|
       klass, name = class_dot_name.split('.')
-      m = klass.constantize.public_send name.to_sym
+      m = const_get(klass.to_sym).public_send name.to_sym
     end
   end
 
